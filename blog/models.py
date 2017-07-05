4b4b0d8cea96f654717a6ece3921a2f57f9f3170
Your Name <you@example.com>
2017-07-06 02:32:23 +0800

    changed name

--- blog/models.py
+++ blog/models.py
@@ -9,15 +9,17 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    department = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    department = models.TextField()
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+
 
     def __str__(self):
         return self.title
