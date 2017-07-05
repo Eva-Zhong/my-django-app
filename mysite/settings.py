15de717e081bb0579835a42be360efeac6c543cf
Your Name <you@example.com>
2017-07-06 02:07:10 +0800

    add allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['carlofficehour.pythonanywhere.com',
                  '127.0.0.1',
+                 'CarlOfficeHourAdmin.pythonanywhere.com',
                 ]
 
 
